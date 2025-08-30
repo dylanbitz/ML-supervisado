18c162771e1602c44c18de857d499aa5e05bb74f
dbitz <dbielosp@gmail.com>
2025-08-30 17:33:48 -0500

    add link variable of the styles.css and add route caso1-

--- app.py
+++ app.py
@@ -5,10 +5,18 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # toca aprender bootstrap
+    link_estilos = "./static/styles.css"
     return render_template(
         "index.html",
+        link_estilos,
         presentation="Bienvenidos a mi página web",
         titulo_actividad="WebApp Flask",
+    )
+
+@app.route('/caso1-')
+def caso1():
+    return render_template(
+        "caso1-",
     )
 
 if __name__ == '__main__':
