a74dabf6ff946b9c64179be5d70fbbda26e42fdf
dbitz <dbielosp@gmail.com>
2025-08-31 19:47:17 -0500

    add the rest of the endpoints of the use cases

--- app.py
+++ app.py
@@ -2,20 +2,50 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.get('/')
 def index():
-    link_estilos = "./static/styles.css" #tienen que modificar el último nombre styles.css a su propio archivo css
+    link_estilos = "../static/css/index.css" #tienen que modificar el último nombre styles.css a su propio archivo css
     return render_template(
         "index.html",
         link=link_estilos,
         titulo="ML Supervisado",
-        presentation="Bienvenidos a mi página web",
+        
     )
 
-@app.route('/caso1-')
+@app.get('/caso1-')
 def caso1():
+    link_estilos = "../static/css/caso1-.css" #tienen que modificar el último nombre styles.css a su propio archivo css
     return render_template(
         "caso1-.html",
+        link=link_estilos,
+        
+    )
+
+@app.get('/caso2-')
+def caso2():
+    link_estilos = "../static/css/caso2-.css" #tienen que modificar el último nombre styles.css a su propio archivo css
+    return render_template(
+        "caso2-.html",
+        link=link_estilos,
+        
+    )
+
+@app.get('/caso3-')
+def caso3():
+    link_estilos = "../static/css/caso3-.css" #tienen que modificar el último nombre styles.css a su propio archivo css
+    return render_template(
+        "caso3-.html",
+        link=link_estilos,
+        
+    )
+
+@app.get('/caso4-')
+def caso4():
+    link_estilos = "../static/css/caso4-.css" #tienen que modificar el último nombre styles.css a su propio archivo css
+    return render_template(
+        "caso4-.html",
+        link=link_estilos,
+        
     )
 
 if __name__ == '__main__':
