d3fcdf47149de81f0784353df84339e1be7b5abb
dbitz <dbielosp@gmail.com>
2025-09-05 15:45:26 -0500

    Revert "Merge pull request #1 from dylanbitz/A3_CasosUsoMLSupervisado"
    This reverts commit 996039612fabb73123c548f8b105b12a65bc4362, reversing
    changes made to 97f5bdcf03d6026746c6ee9c5e139a45ad8537b8.

--- app.py
+++ app.py
@@ -2,51 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.get('/')
+@app.route('/')
 def index():
-    link_estilos = "../static/css/index.css" #tienen que modificar el último nombre styles.css a su propio archivo css
+    # toca aprender bootstrap
+    link_estilos = "./static/styles.css"
     return render_template(
         "index.html",
-        link=link_estilos,
-        titulo="ML Supervisado",
-        
+        link_estilos,
+        presentation="Bienvenidos a mi página web",
+        titulo_actividad="WebApp Flask",
     )
 
-@app.get('/caso1-indmusical')
+@app.route('/caso1-')
 def caso1():
-    link_estilos = "../static/css/caso1-.css" #tienen que modificar el último nombre styles.css a su propio archivo css
     return render_template(
-        "caso1-indmusical.html",
-        link=link_estilos,
-        titulo="Caso 1 Industria Musical"
-        
-    )
-
-@app.get('/caso2-IndTexti')
-def caso2():
-    link_estilos = "../static/css/caso2-.css" #tienen que modificar el último nombre styles.css a su propio archivo css
-    return render_template(
-        "caso2-.html",
-        link=link_estilos,
-        
-    )
-
-@app.get('/caso3-')
-def caso3():
-    link_estilos = "../static/css/caso3-.css" #tienen que modificar el último nombre styles.css a su propio archivo css
-    return render_template(
-        "caso3-.html",
-        link=link_estilos,
-        
-    )
-
-@app.get('/caso4-')
-def caso4():
-    link_estilos = "../static/css/caso4-.css" #tienen que modificar el último nombre styles.css a su propio archivo css
-    return render_template(
-        "caso4-.html",
-        link=link_estilos,
-        
+        "caso1-",
     )
 
 if __name__ == '__main__':
